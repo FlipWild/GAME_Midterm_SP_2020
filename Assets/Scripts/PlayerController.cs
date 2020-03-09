03470d21bc682419df6806719b1f887e709c2387
DanS <DSumbillo@colum.edu>
2020-03-08 21:35:49 -0500

    Base Gameplay
    Implemented all features for basic gameplay

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    public float horizonralInput;
+    public float speed = 10.0f;
+    public float maxHealth = 100;
+    public float healthDecreaseSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            if (maxHealth >= 75)
+            {
+                jumpForce = 10.0f;
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isOnGround = false;
+            }
+            if (maxHealth > 50 && maxHealth < 75)
+            {
+                jumpForce = 7.0f;
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isOnGround = false;
+            }
+            if (maxHealth < 50)
+            {
+                jumpForce = 5.0f;
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isOnGround = false;
+            }
+        }
+
+        horizonralInput = Input.GetAxis("Horizontal");
+        if (!gameOver)
+        {
+            transform.Translate(Vector3.right * horizonralInput * Time.deltaTime * speed);
+        }
+
+        maxHealth -= Time.deltaTime * healthDecreaseSpeed;
+        if (maxHealth == 0)
+        {
+            gameOver = true;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            maxHealth -= 25;
+            Destroy(GameObject.FindWithTag("Obstacle"));
+        }
+        else if (collision.gameObject.CompareTag("Item"))
+        {
+            maxHealth += 25;
+            Destroy(GameObject.FindWithTag("Item"));
+        }
+        else if (collision.gameObject.CompareTag("Fire"))
+        {
+            gameOver = true;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, Screen.width, Screen.height), $"Health: {maxHealth}%");
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    private Vector3 spawnPos = new Vector3(25, 1, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Look up random time lag
+
+    }
+
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            int times = Random.Range(0, obstaclePrefabs.Length);
+
+            for (int i = 0; i < times; i++)
+            {
+                int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+                Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+            }
+
+
+        }
+    }
+}
